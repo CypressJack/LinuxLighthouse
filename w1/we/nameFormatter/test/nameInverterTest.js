79207fdc9d90a53ae186926f94e52f66eeef80ae
archaemedes <wadecroft@gmail.com>
2021-05-02 16:56:40 +0000

    Continued working on nameInverter and test cases

--- w1/we/nameFormatter/test/nameInverterTest.js
+++ w1/we/nameFormatter/test/nameInverterTest.js
@@ -17,4 +17,46 @@ describe('nameInverter', function() {
     assert.isTrue(inverter === "name");
   });
 
+  it("should return a single name when passed a single name with extra spaces", function() {
+    const name = " name "
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "name");
+  });
+
+  it("should return a last-name, first-name when passed a first and last-name", function() {
+    const name = "first last"
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "last, first");
+  });
+
+  it("should return an empty string when passed a single honorific", function() {
+    const name = "Dr. "
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "");
+  });
+
+  it("should return honorific first-name when passed honorific first-name", function() {
+    const name = "Dr. first"
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "Dr. first");
+  });
+
+  it("should return a honorific last-name, first-name when passed honorific first-name last-name", function() {
+    const name = "Dr. first-name last-name"
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "Dr. last-name, first-name");
+  });
+
+  it("should return a honorific last-name, first-name when passed honorific first-name last-name with extra spaces around the words", function() {
+    const name = " Dr. first-name last-name "
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "Dr. last-name, first-name");
+  });
+
+  it("should throw an error when name is undefined", function() {
+    const name = " Dr. first-name last-name "
+    const inverter = nameInverter(name);
+    assert.isTrue(inverter === "Dr. last-name, first-name");
+  });
+
 });
