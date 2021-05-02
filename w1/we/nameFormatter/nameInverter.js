34912d227831169256ed2cad841f93cbc145eaa2
archaemedes <wadecroft@gmail.com>
2021-05-02 10:10:01 -0700

    Working on nameInverter

--- w1/we/nameFormatter/nameInverter.js
+++ w1/we/nameFormatter/nameInverter.js
@@ -1,16 +1,19 @@
 const nameInverter = function(name) {
-  console.log(name.charAt(-1));
+  console.log(`String is: "${name.slice(1, -1)}"`);
+  console.log(`0th character is: "${name.charAt(0)}"`);
+  console.log(`last character is: "${name.charAt(name.length - 1)}"`);
   if (name === "") {
     return "";
   } else if (!name.includes(" ")) {
     return name;
-  } else if (name.charAt(0) === " " && name.charAt(-1) === " ") {
-    return name.slice(0, -1);
+  } else if (name.charAt(0) === " " && name.charAt(name.length - 1) === " ") {
+    console.log(`Return name without spaces "${name.slice(1, name.length - 1)}"`);
+    return name.slice(1, name.length - 1);
   }
 }
 
 
-console.log(`Name Inverter Returns: ${nameInverter("")}`);
+console.log(`Name Inverter Returns: ${nameInverter(" name ")}`);
 
 
 module.exports = nameInverter;
