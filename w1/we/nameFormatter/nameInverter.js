020e0cbc740014abc461686b9e98694da17d50ea
archaemedes <wadecroft@gmail.com>
2021-05-02 10:35:59 -0700

    Working on nameInverter function

--- w1/we/nameFormatter/nameInverter.js
+++ w1/we/nameFormatter/nameInverter.js
@@ -1,19 +1,26 @@
 const nameInverter = function(name) {
-  console.log(`String is: "${name.slice(1, -1)}"`);
-  console.log(`0th character is: "${name.charAt(0)}"`);
-  console.log(`last character is: "${name.charAt(name.length - 1)}"`);
+  let indexOfSpace = name.indexOf(" ");
+  let lastIndexOfSpace = name.lastIndexOf(" ");
+  let length = name.length;
+  console.log(`String is: "${name}`);
+  console.log(`first name is: "${name.slice(0, indexOfSpace)}"`);
+  console.log(`last name is: "${name.slice(indexOfSpace + 1, length)}"`);
   if (name === "") {
     return "";
   } else if (!name.includes(" ")) {
     return name;
   } else if (name.charAt(0) === " " && name.charAt(name.length - 1) === " ") {
-    console.log(`Return name without spaces "${name.slice(1, name.length - 1)}"`);
     return name.slice(1, name.length - 1);
+  } else if (indexOfSpace !== 0 && lastIndexOfSpace !== name.length - 1) {
+    let firstName = name.slice(0, indexOfSpace);
+    let lastName = name.slice(indexOfSpace + 1, length);
+    console.log(`${firstName}, ${lastName}`);
+    return `${lastName}, ${firstName}`;
   }
 }
 
 
-console.log(`Name Inverter Returns: ${nameInverter(" name ")}`);
+console.log(`Name Inverter Returns: ${nameInverter("first last")}`);
 
 
 module.exports = nameInverter;
